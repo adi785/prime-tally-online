74eb09b7ec0bba0beb4c6b92bb7efbba75d4d10c
[dyad] <adityafalor79@gmail.com>
2025-12-26 21:54:03 +0530

    [dyad] Fixed import error in ResetPasswordPage.tsx - wrote 1 file(s)

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,4 +1,4 @@
-import { ResetPassword } from '@/components/auth/ResetPassword';
+import ResetPassword from '@/components/auth/ResetPassword';
 
 export default function ResetPasswordPage() {
   return <ResetPassword />;
